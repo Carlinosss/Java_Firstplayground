ae5d53719b6afeb747196806a71e14fe07873b79
Carlinosss <92334798+Carlinosss@users.noreply.github.com>
2022-01-06 07:52:48 +0100

    Add files via upload

---
+++ Javacopy.js
@@ -0,0 +1,25 @@
+console.log("Hello Winc Academy");
+let myname = 'carlijn';
+console.log(myname);
+
+let x = 4 + 4;
+console.log(x);
+
+let y = "4" + "4";
+console.log(y);
+
+let plus = 6 + 4;
+console.log(plus);
+
+console.log(50 - 38);
+
+let multi = 2 * 12;
+console.log(multi);
+
+console.log(56 / 8);
+
+let percen = 5;
+let tage = percen % 2;
+console.log(tage);
+
+console.log(typeof 'forty');
